8b0d29db7ec9ed2db4dd04b11536016aa9cc7e9c
devknk <knkrajewska@gmail.com>
2024-04-20 11:03:06 +0200

    Create doctors microsrvice.

---
+++ med_reservations/doctors/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Doctor(models.Model):
+    pass
---
+++ med_reservations/doctors/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def doctors(request):
+    return HttpResponse("Hello doctors!")
