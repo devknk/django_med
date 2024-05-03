f0b8ac50bf83ddd682a64fc743ab668980f3e9e2
devknk <knkrajewska@gmail.com>
2024-05-03 15:25:33 +0200

    Swiping around, merge admin to one admin.py file under \account

--- src/account/admin.py
+++ src/account/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from account.models import Account
+from app.models import Doctor, Patient, Visit
+
+
 
 class AccountAdmin(UserAdmin):
     list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin', 'is_staff')
@@ -8,7 +11,25 @@ class AccountAdmin(UserAdmin):
     readonly_fields = ('id', 'date_joined', 'last_login')
 
     filter_horizontal = ()
-    list_filter =()
+    list_filter = ()
     fieldsets = ()
 
+
+class MyDoctors(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "speciality", "added_date")
+
+
+class MyVisits(admin.ModelAdmin):
+    list_display = ("id", "date", "doctor", "patient", "created_date")
+    readonly_fields = ["created_date", "speciality"]
+
+
+class MyPatients(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "added_date")
+
+
 admin.site.register(Account, AccountAdmin)
+
+admin.site.register(Doctor, MyDoctors)
+admin.site.register(Patient, MyPatients)
+admin.site.register(Visit, MyVisits)
--- src/app/admin.py
+++ src/app/admin.py
@@ -1,18 +1,1 @@
 from django.contrib import admin
-from app.models import Doctor, Patient, Visit
-
-
-class MyDoctors(admin.ModelAdmin):
-    list_display = ("id", "first_name", "last_name", "speciality", "added_date")
-
-class MyVisits(admin.ModelAdmin):
-    list_display = ("id", "date", "doctor", "patient", "created_date")
-    readonly_fields = ["created_date", "speciality"]
-
-class MyPatients(admin.ModelAdmin):
-    list_display = ("id", "first_name", "last_name", "added_date")
-
-
-admin.site.register(Doctor, MyDoctors)
-admin.site.register(Patient, MyPatients)
-admin.site.register(Visit, MyVisits)
